9a00240e51d6e3883a7715a419394f24f7fc1ffe
acidicMercury8 <acidicmercury8@outlook.com>
2026-04-20 19:21:49 +0300

    Add Windows project

---
+++ src/ImeSense.Boilerplates.Avalonia.Windows/Program.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
+
+using System;
+
+namespace ImeSense.Boilerplates.Avalonia.Windows;
+
+internal class Program
+{
+    /// <summary>
+    /// Initialization code.
+    /// </summary>
+    /// <param name="args"></param>
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
+
+    /// <summary>
+    /// Avalonia configuration. Also used by visual designer.
+    /// </summary>
+    /// <returns></returns>
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .UseWin32()
+            .UseReactiveUI()
+            .LogToTrace();
+    }
+}
